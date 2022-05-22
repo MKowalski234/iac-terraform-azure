6812f8c2681c23e619a9642910fe4ad8b2b783a3
MKowalski234 <58140230+MKowalski234@users.noreply.github.com>
2022-05-22 11:23:14 +0200

    Update main.tf

--- azure-blob-storage/main.tf
+++ azure-blob-storage/main.tf
@@ -1,26 +1,26 @@
 resource "azurerm_resource_group" "example" {
-  name     = "example-resources"
-  location = "West Europe"
+  name     = var."example-resources"
+  location = var."North Europe"
 }
 
 resource "azurerm_storage_account" "example" {
-  name                     = "examplestoracc"
+  name                     = var.examplestoracc
   resource_group_name      = azurerm_resource_group.example.name
   location                 = azurerm_resource_group.example.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
+  account_tier             = var.Standard
+  account_replication_type = var.LRS
 }
 
 resource "azurerm_storage_container" "example" {
-  name                  = "content"
+  name                  = var."content"
   storage_account_name  = azurerm_storage_account.example.name
-  container_access_type = "private"
+  container_access_type = var.private
 }
 
 resource "azurerm_storage_blob" "example" {
-  name                   = "my-awesome-content.zip"
+  name                   = var.my-awesome-content.zip
   storage_account_name   = azurerm_storage_account.example.name
   storage_container_name = azurerm_storage_container.example.name
-  type                   = "Block"
-  source                 = "some-local-file.zip"
+  type                   = var.Block
+  source                 = var.some-local-file.zip
 }
