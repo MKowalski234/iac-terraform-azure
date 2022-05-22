6e46dd6161b5661a850c02fed7927db0224673fd
MKowalski234 <58140230+MKowalski234@users.noreply.github.com>
2022-05-22 10:44:13 +0200

    Update variables.tf

--- azure-functions/variables.tf
+++ azure-functions/variables.tf
@@ -1,1 +1,21 @@
 
+variable "resource_group" {
+  type = string
+  default = "test"
+}
+variable "storage_account"{
+  type = string
+  default = "test"
+}
+variable "service_plan"{
+  type = string
+  default = "test"
+}
+variable "linux_function_app"{
+  type = string
+  default = "test"
+}
+variable "function_app"{
+  type = string
+  default = "test"
+}
