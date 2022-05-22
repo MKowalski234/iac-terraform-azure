4792af7d2d90dac942dd6ce9a00f242d1eb9db1d
MKowalski234 <58140230+MKowalski234@users.noreply.github.com>
2022-05-22 11:23:13 +0200

    Update main.tf

--- azure-functions/main.tf
+++ azure-functions/main.tf
@@ -1,25 +1,25 @@
-resource "azurerm_resource_group" "rg" {
-  name     = var."example-resources"
-  location = var."North Europe"
+ resource "azurerm_resource_group" "rg" {
+  name     = var.example_resources
+  location = var.North Europe
 }
 
 resource "azurerm_storage_account" "ra" {
-  name                     = var."storageaccountname"
+  name                     = var.storageaccountname
   resource_group_name      = azurerm_resource_group.rg.name
   location                 = azurerm_resource_group.rg.location
-  account_tier             = var."FREE"
-  account_replication_type = var."GRS"
+  account_tier             = var.FREE
+  account_replication_type = var.GRS
 
 
 resource "azurerm_app_service_plan" "rp" {
-  name                = var."api-appserviceplan-pro"
+  name                = var.api-appserviceplan-pro
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 
 }
 
 resource "azurerm_linux_function_app" "example" {
-  name                = "example-function-app"
+  name                = example-function-app
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   service_plan_id     = azurerm_service_plan.example.id
