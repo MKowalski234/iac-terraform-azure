0249e6ce200195278af7d1bd01cc39b8076ddbb5
MKowalski234 <58140230+MKowalski234@users.noreply.github.com>
2022-05-22 10:44:01 +0200

    Update main.tf

--- azure-functions/main.tf
+++ azure-functions/main.tf
@@ -1,30 +1,23 @@
 resource "azurerm_resource_group" "rg" {
-  name     = "example-resources"
-  location = "West Europe"
+  name     = var."example-resources"
+  location = var."North Europe"
 }
 
 resource "azurerm_storage_account" "ra" {
-  name                     = "storageaccountname"
-  resource_group_name      = azurerm_resource_group.ra.name
-  location                 = azurerm_resource_group.ra.location
-  account_tier             = "Standard"
-  account_replication_type = "GRS"
+  name                     = var."storageaccountname"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = var."FREE"
+  account_replication_type = var."GRS"
 
 
 resource "azurerm_app_service_plan" "rp" {
-  name                = "api-appserviceplan-pro"
-  location            = azurerm_resource_group.rp.location
-  resource_group_name = azurerm_resource_group.rp.name
+  name                = var."api-appserviceplan-pro"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
 
 }
 
-resource "azurerm_linux_function_app" "example" {
-  name                = "example-linux-function-app"
-  resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_resource_group.example.location
-
-  storage_account_name = azurerm_storage_account.example.name
-  service_plan_id      = azurerm_service_plan.example.id
 resource "azurerm_linux_function_app" "example" {
   name                = "example-function-app"
   location            = azurerm_resource_group.example.location
@@ -40,4 +33,3 @@ resource "azurerm_linux_function_app" "example" {
     }
   }
 }
-  
