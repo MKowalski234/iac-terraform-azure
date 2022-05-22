3b50c90d330aa611928e2b9c2b03ac7f98f4d6a3
MKowalski234 <58140230+MKowalski234@users.noreply.github.com>
2022-05-22 11:12:32 +0200

    Update variables.tf

--- azure-blob-storage/variables.tf
+++ azure-blob-storage/variables.tf
@@ -1,1 +1,20 @@
 
+variable "resource_group"{
+  type = string
+  default = "test"
+}
+
+variable "storage_account"{
+  type = string
+  default = "test"
+}
+
+variable "storage_container"{
+  type = string
+  default = "test"
+}
+
+variable "storage_blob"{
+  type = string
+  default = "test"
+}
